d6bca400a4c270ad533be5a77ead6127f0e2edc3
Christopher Medrano <me@chrismedrano.com>
2022-05-27 13:07:25 -0400

    changed allowed hostname on bokeh server

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def favicon():
 def bk_worker():
     # Can't pass num_procs > 1 in this configuration. If you need to run multiple
     # processes, see e.g. flask_gunicorn_embed.py
-    server = Server({'/bkapp': bkapp}, io_loop=IOLoop(), allow_websocket_origin=["127.0.0.1:8000"])
+    server = Server({'/bkapp': bkapp}, io_loop=IOLoop(), allow_websocket_origin=["0.0.0.0:8000"])
     server.start()
     server.io_loop.start()
 
